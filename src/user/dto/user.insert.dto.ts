f33180a3d1786abf176722ca0b9333580eb78d6f
Minu Song <smw0807@gmail.com>
2023-11-17 21:38:50 +0900

    reactor: username ->userName

--- src/user/dto/user.insert.dto.ts
+++ src/user/dto/user.insert.dto.ts
@@ -3,7 +3,7 @@ import { Field, InputType } from '@nestjs/graphql';
 @InputType()
 export class UserInsertDto {
   @Field({ description: '유저 이름' })
-  username: string;
+  userName: string;
 
   @Field({ description: '유저 이메일' })
   email: string;
