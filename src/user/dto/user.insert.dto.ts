50714f269af67169921ad3b626b34374cd35003a
Minu Song <smw0807@gmail.com>
2023-11-17 21:35:35 +0900

    fix: 유저 등록에 필요한 필드 잘못된 부분 수정 name -> username

--- src/user/dto/user.insert.dto.ts
+++ src/user/dto/user.insert.dto.ts
@@ -2,9 +2,9 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class UserInsertDto {
-  @Field()
-  name: string;
+  @Field({ description: '유저 이름' })
+  username: string;
 
-  @Field()
+  @Field({ description: '유저 이메일' })
   email: string;
 }
