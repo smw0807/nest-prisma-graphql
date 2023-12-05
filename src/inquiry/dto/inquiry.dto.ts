c7bc4bb304c813b5922646fc8a4cc845e2136765
Minu Song <smw0807@gmail.com>
2023-12-06 00:35:30 +0900

    remove: 파일삭제 공식문서 기준으로 만들어보기 위해 삭제

--- src/inquiry/dto/inquiry.dto.ts
+++
@@ -1,6 +0,0 @@
-import { InputType, Field, Int } from '@nestjs/graphql';
-
-@InputType()
-export class InqueryFindDto {
-  // @Field()
-}
--- src/user/dto/user.search.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-
-@InputType({ description: '유저 아이디 정보 가져오기' })
-export class UserFindById {
-  @Field({ description: '유저 아이디' })
-  userId: number;
-}
