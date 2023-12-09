ecae6aefff03a96b174d2d72d7009d1caf770ac2
Minu Song <smw0807@gmail.com>
2023-12-09 15:44:47 +0900

    rename: prisma 모율 위치 변경 여러 모듈에서 사용할 것 같은 것들은 common 디렉터리에서 관리하기 위해 옮김

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
 
 @Module({
   providers: [UserService, UserResolver],
