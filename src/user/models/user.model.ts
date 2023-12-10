dab77aba28a6191f6853475c761dc88502dbdf33
Minu Song <smw0807@gmail.com>
2023-12-11 00:19:11 +0900

    refator: User Model 변경 프리즈마 스키마 변경으로 인한 수정

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,13 +1,27 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, registerEnumType } from '@nestjs/graphql';
+import { Role } from '@prisma/client';
 
-@ObjectType()
+registerEnumType(Role, { name: 'Role' });
+@ObjectType({ description: '사용자' })
 export class User {
-  @Field(() => Int)
-  id: number;
+  @Field(() => Int, { description: '아이디' })
+  readonly id: number;
 
-  @Field()
-  email: string;
+  @Field(() => String, { description: '이메일' })
+  readonly email: string;
 
-  @Field({ nullable: true })
-  name?: string;
+  @Field({ nullable: true, description: '이름' })
+  readonly name?: string;
+
+  @Field(() => Int, { description: '프로필 조회수' })
+  readonly profileViews: number;
+
+  @Field(() => Role, { description: '역할' })
+  readonly role: Role;
+
+  @Field(() => Boolean, { nullable: true, description: '??' })
+  readonly coinflips?: boolean;
+
+  // readonly posts;
+  // readonly profile?;
 }
